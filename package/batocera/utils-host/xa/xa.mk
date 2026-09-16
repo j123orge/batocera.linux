c3002795fb0d3f936049c81bcdeb7f4a4e403b53
Jorge Paulo <j123orge@gmail.com>
2026-09-16 12:54:56 -0300

    xa: build host objects explicitly

--- package/batocera/utils-host/xa/xa.mk
+++ package/batocera/utils-host/xa/xa.mk
@@ -9,7 +9,20 @@ XA_SOURCE=xa-$(XA_VERSION).tar.gz
 XA_SITE = https://www.floodgap.com/retrotech/xa/dists
 
 define HOST_XA_BUILD_CMDS
-	$(HOST_MAKE_ENV) $(MAKE) -C $(@D) -f Makefile
+	cd $(@D)/src && \
+	for src in \
+		xa.c xaa.c xal.c xap.c xat.c xar.c xar2.c \
+		xao.c xau.c xam.c xacharset.c xalisting.c; \
+	do \
+		obj="$${src%.c}.o"; \
+		$(HOSTCC_NOCCACHE) $(HOST_CFLAGS) -I. \
+			-c "$$src" -o "$$obj" || exit 1; \
+	done
+	cd $(@D)/src && \
+		$(HOSTCC_NOCCACHE) $(HOST_LDFLAGS) \
+		-o ../xa \
+		xa.o xaa.o xal.o xap.o xat.o xar.o xar2.o \
+		xao.o xau.o xam.o xacharset.o xalisting.o
 endef
 
 define HOST_XA_INSTALL_CMDS
